75b4f18863f32ffedcf45e4be34a9a290fa6d6a2
Fritz Schimpf <fritz@schimpf-ing.no>
2015-01-16 15:25:40 +0100

    generateSCL.py schreibt header und footer.

--- generateSCL.py
+++ generateSCL.py
@@ -3,6 +3,17 @@ def generateSCL(filename, stepList, transitionList, digInDict, digOutDict):
     # create output file and write header. FIX: ask for overwrite:
     outfilename = filename.partition('.')[0] + '.scl'
     outfile = open(outfilename, 'w')
- 
+
+    outfile.write ('FUNCTION_BLOCK "{0}"\n'.format(filename.partition('.')[0]))
+    outfile.write ('VERSION : 0.1\n')
+
+    # create input veriables for block:
+    outfile.write ('    VAR_INPUT\n')
+
+    outfile.write ('    END_VAR\n')
+
+
+    # write footer and close file
+    outfile.write('END_FUNCTION_BLOCK\n')
     outfile.close()
     print '.scl generated successfully. :-)'
