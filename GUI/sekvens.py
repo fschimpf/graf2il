fe27ce03164c9cd9df16bcd8a7eae2894986ea40
Fritz Schimpf <fritz@schimpf-ing.no>
2015-04-06 20:56:57 +0200

    First version of GUI finished and working. Supports only .awl-output.

--- GUI/sekvens.py
+++ GUI/sekvens.py
@@ -13,21 +13,22 @@ class MeinProgramm(QtGui.QMainWindow, Ui_MainWindow):
         XMLFileToCompileName = 'test'
             
         # connect slots for GUI-elements
-        self.connect(self.pushButton, QtCore.SIGNAL('clicked ()'), self.startJGrafchart)
         self.connect(self.pushButton_2, QtCore.SIGNAL('clicked (bool)'), self.start_graf2il)        
-        self.connect(self.pushButton_3, QtCore.SIGNAL('clicked (bool)'), self.chooseInFile)   
+        self.connect(self.pushButton_3, QtCore.SIGNAL('clicked (bool)'), self.chooseInFile)  
 
-    def startJGrafchart(self):
-        self.statusBar().showMessage('starter JGrafchart')
+        self.statusBar().showMessage('Malvik VGS - Fritz Schimpf') 
 
     def start_graf2il(self):
-        self.statusBar().showMessage('starter graf2il')
+        self.statusBar().showMessage('Script graf2il aktiv')
         graf2il.main(self.XMLFileToCompileName,'awl')
+        self.statusBar().showMessage('Malvik VGS - Fritz Schimpf')
 
     def chooseInFile(self):
         self.statusBar().showMessage('Velg inputfil')
-        self.XMLFileToCompileName = str(QtGui.QFileDialog.getOpenFileName(self, 'Velg fil som skal bli oversatt', '', selectedFilter='*.xml'))
+        #self.XMLFileToCompileName = str(QtGui.QFileDialog.getOpenFileName(self, 'Velg fil som skal bli oversatt', '', selectedFilter='*.xml'))
+        self.XMLFileToCompileName = str(QtGui.QFileDialog.getOpenFileName(self, 'Velg fil som skal bli oversatt', '', 'JGrafchart XML-filer (*.xml);;alle filer (*.*)'))
         self.lineEdit.setText(self.XMLFileToCompileName)
+        self.statusBar().showMessage('Malvik VGS - Fritz Schimpf')
         
 
 app = QtGui.QApplication(sys.argv)
--- GUI/sekvens_gui.py
+++ GUI/sekvens_gui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'sekvens_gui.ui'
 #
-# Created: Thu Dec 11 23:40:38 2014
+# Created: Mon Apr  6 20:21:10 2015
 #      by: PyQt4 UI code generator 4.10.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -26,7 +26,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(732, 350)
+        MainWindow.resize(823, 502)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
@@ -58,17 +58,6 @@ class Ui_MainWindow(object):
         self.label_4.setObjectName(_fromUtf8("label_4"))
         self.verticalLayout_2.addWidget(self.label_4)
         self.horizontalLayout.addLayout(self.verticalLayout_2)
-        spacerItem = QtGui.QSpacerItem(4, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.pushButton = QtGui.QPushButton(self.frame)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy)
-        self.pushButton.setMinimumSize(QtCore.QSize(138, 0))
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.horizontalLayout.addWidget(self.pushButton)
         self.verticalLayout.addWidget(self.frame)
         self.frame_2 = QtGui.QFrame(self.centralwidget)
         self.frame_2.setFrameShape(QtGui.QFrame.StyledPanel)
@@ -105,11 +94,21 @@ class Ui_MainWindow(object):
         self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
         self.horizontalLayout_2.addWidget(self.pushButton_3)
         self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout_5 = QtGui.QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(_fromUtf8("horizontalLayout_5"))
+        self.label_6 = QtGui.QLabel(self.frame_2)
+        self.label_6.setObjectName(_fromUtf8("label_6"))
+        self.horizontalLayout_5.addWidget(self.label_6)
+        self.comboBox = QtGui.QComboBox(self.frame_2)
+        self.comboBox.setObjectName(_fromUtf8("comboBox"))
+        self.comboBox.addItem(_fromUtf8(""))
+        self.horizontalLayout_5.addWidget(self.comboBox)
+        self.verticalLayout_3.addLayout(self.horizontalLayout_5)
         self.horizontalLayout_4.addLayout(self.verticalLayout_3)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem)
         spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.horizontalLayout_4.addItem(spacerItem1)
-        spacerItem2 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout_4.addItem(spacerItem2)
         self.pushButton_2 = QtGui.QPushButton(self.frame_2)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
@@ -125,6 +124,8 @@ class Ui_MainWindow(object):
         self.frame_3.setObjectName(_fromUtf8("frame_3"))
         self.horizontalLayout_3 = QtGui.QHBoxLayout(self.frame_3)
         self.horizontalLayout_3.setObjectName(_fromUtf8("horizontalLayout_3"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout()
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.label_3 = QtGui.QLabel(self.frame_3)
         font = QtGui.QFont()
         font.setPointSize(14)
@@ -132,40 +133,52 @@ class Ui_MainWindow(object):
         font.setWeight(75)
         self.label_3.setFont(font)
         self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.horizontalLayout_3.addWidget(self.label_3)
+        self.verticalLayout_4.addWidget(self.label_3)
+        self.label_7 = QtGui.QLabel(self.frame_3)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label_7.sizePolicy().hasHeightForWidth())
+        self.label_7.setSizePolicy(sizePolicy)
+        self.label_7.setMinimumSize(QtCore.QSize(529, 102))
+        self.label_7.setWordWrap(True)
+        self.label_7.setObjectName(_fromUtf8("label_7"))
+        self.verticalLayout_4.addWidget(self.label_7)
+        self.horizontalLayout_3.addLayout(self.verticalLayout_4)
         self.verticalLayout.addWidget(self.frame_3)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 732, 25))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 823, 25))
         self.menubar.setObjectName(_fromUtf8("menubar"))
-        self.menuInfo = QtGui.QMenu(self.menubar)
-        self.menuInfo.setObjectName(_fromUtf8("menuInfo"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
         self.statusbar.setObjectName(_fromUtf8("statusbar"))
         MainWindow.setStatusBar(self.statusbar)
         self.actionInfo = QtGui.QAction(MainWindow)
         self.actionInfo.setObjectName(_fromUtf8("actionInfo"))
-        self.menubar.addAction(self.menuInfo.menuAction())
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(_translate("MainWindow", "Lag en sekvensstyring - GUI til graf2il", None))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Oversett sekvensstyring fra JGrafchart til Siemens Step7", None))
         self.label.setText(_translate("MainWindow", "1. Lag sekvensstyring i JGrafchart. ", None))
         self.label_4.setText(_translate("MainWindow", "Tipps:\n"
 " - Elementer du kan bruke er: Input, inverting input, output, inverting output,\n"
 "    initial step, step, transition.\n"
-" - Lagre filen som .xml\n"
-" - Du må allted ha med minst en \"initial step\" og en \"step\"\n"
-" - Bruk \"compile\" knappen for å sjekke om det fins feil før du prøver å oversette.", None))
-        self.pushButton.setText(_translate("MainWindow", "Start JGrafchart", None))
+" - Lagre filen som .xml.\n"
+" - Du må ha minst en \"initial step\" og en \"step\".\n"
+" - Bruk \"compile\" knappen i JGrafchart for å sjekke om det fins feil før du\n"
+"   prøver å oversette.", None))
         self.label_2.setText(_translate("MainWindow", "2. Oversett til Step 7", None))
         self.label_5.setText(_translate("MainWindow", "JGrafchart-fil (.xml)", None))
         self.pushButton_3.setText(_translate("MainWindow", "Velg", None))
+        self.label_6.setText(_translate("MainWindow", "oversett til PLS-språk (type PLS)", None))
+        self.comboBox.setItemText(0, _translate("MainWindow", ".awl (S7-200)", None))
         self.pushButton_2.setText(_translate("MainWindow", "Oversett til Step7", None))
-        self.label_3.setText(_translate("MainWindow", "3. Nesten ferdig. Start Step7, gå til \"File - Import\" og last inn .awl-filen! ", None))
-        self.menuInfo.setTitle(_translate("MainWindow", "Info", None))
+        self.label_3.setText(_translate("MainWindow", "3. Importer fil til Step 7 ", None))
+        self.label_7.setText(_translate("MainWindow", " - Start Step 7,\n"
+" - gå til \"File - Import\" og last inn den nye .awl-filen.\n"
+" - Last programmet inn i PLSen på vanllig måte og test!", None))
         self.actionInfo.setText(_translate("MainWindow", "Info", None))
 
